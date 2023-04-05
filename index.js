7c760d06064be1d6cd20ae3e7370540724020dec
dabasishJoy <dabasish@6sensehq.com>
2023-04-05 10:42:52 +0600

    create server and run the server

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Node.js");
+// dependecies
+const http = require("http");
+
+//module scafolding - creating an app object
+const app = {};
+
+// configuration
+app.config = {
+  port: 5000,
+};
+
+// crete server method
+app.createServer = () => {
+  // creating server using http module
+  const server = http.createServer(app.handleReqRes);
+  // emit event
+  server.listen(app.config.port, () => {
+    console.log(`Server is listeing on port ${app.config.port}`);
+  });
+};
+
+// hanlde req and res
+app.handleReqRes = (req, res) => {
+  res.end("Hello World");
+};
+
+// run the application
+app.createServer();
---
+++ practice/practice.js
@@ -0,0 +1,5 @@
+const path = require("path");
+
+const myPath = "E:ProjectsLearn-NodeJSpractice";
+
+console.log(path.parse(myPath));
