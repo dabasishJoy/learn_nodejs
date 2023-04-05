f6a127ab4efb8708695baa7ee6215172274ac9ad
dabasishJoy <dabasish@6sensehq.com>
2023-04-05 15:11:52 +0600

    token handlder added

--- helpers/utilities.js
+++ helpers/utilities.js
@@ -20,9 +20,9 @@ utilities.parseJSON = (jsonString) => {
 // hash string
 utilities.hash = (str) => {
   if (typeof str === "string" && str.length > 0) {
-    console.log("from Utils", environments, process.env.NODE_ENV);
+    console.log(environments, process.env.NODE_ENV);
     const hash = crypto
-      .createHmac("dabsish758", environments.secretKey)
+      .createHmac("sha256", environments.secretKey)
       .update(str)
       .digest("hex");
     return hash;
@@ -30,5 +30,23 @@ utilities.hash = (str) => {
   return false;
 };
 
+// create random string
+utilities.createRandomString = (strlength) => {
+  let length = strlength;
+  length = typeof strlength === "number" && strlength > 0 ? strlength : false;
+
+  if (length) {
+    const possiblecharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
+    let output = "";
+    for (let i = 1; i <= length; i += 1) {
+      const randomCharacter = possiblecharacters.charAt(
+        Math.floor(Math.random() * possiblecharacters.length)
+      );
+      output += randomCharacter;
+    }
+    return output;
+  }
+  return false;
+};
 // export module
 module.exports = utilities;
--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 const { sampleHandler } = require("./handler/routesHandler/sampleHandler");
 const { userHandler } = require("./handler/routesHandler/userHanlder");
+const { tokenHandler } = require("./handler/routesHandler/tokenHandler");
 
 const routes = {
   sample: sampleHandler,
   user: userHandler,
+  token: tokenHandler,
 };
 module.exports = routes;
