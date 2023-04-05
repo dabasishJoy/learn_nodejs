1ff92a38e24caf79875cdd8e1f3b0e1a9fe8f894
dabasishJoy <dabasish@6sensehq.com>
2023-04-05 22:37:02 +0600

    check monitoring completed

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
-// dependecies
-const http = require("http");
-const { handleReqRes } = require("./helpers/handleReqRes");
-const environment = require("./helpers/environments");
-const { sendTwilioSms } = require("./helpers/notifications");
-const data = require("./lib/data");
+// dependencies
+const server = require("./lib/server");
+const workers = require("./lib/worker");
 
-//module scafolding - creating an app object
+// app object - module scaffolding
 const app = {};
 
-// testing @todo: remove
-sendTwilioSms("01571763498", "Hello world", (err) => {
-  console.log("THis is the error:", err);
-});
-// // testing filesystem
-// data.create(
-//   "test",
-//   "newFile",
-//   { name: "Dabasish", lastname: "Das", Role: "Operational" },
-//   (err) => {
-//     console.log("The error", err);
-//   }
-// );
-
-// // update
-// data.update(
-//   "test",
-//   "newFile",
-//   { name: "Dabasish", lastname: "Joy", Role: "Operational" },
-//   (err) => {
-//     console.log("The error", err);
-//   }
-// );
-
-// // delete file
-// data.delete("test", "newFile", (err) => {
-//   console.log("The error", err);
-// });
-// crete server method
-
-app.createServer = () => {
-  // creating server using http module
-  const server = http.createServer(app.handleReqRes);
-  // emit event
-  server.listen(environment.port, () => {
-    console.log(`Server is listeing on port ${environment.port}`);
-  });
+app.init = () => {
+  // start the server
+  server.init();
+  // start the workers
+  workers.init();
 };
 
-// hanlde req and res
-app.handleReqRes = handleReqRes;
-// run the application
-app.createServer();
+app.init();
+
+// export the app
+module.exports = app;
